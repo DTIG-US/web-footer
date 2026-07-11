b838e7c9c081af38404de807ffcd219b5e82950a
John Aziz <john.aziz@dtig.us>
2026-07-11 18:19:26 -0500

    [Fixed] No data.json import at all, hardcoded © 2022 in HTML

--- footer.component.ts
+++ footer.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as siteData from '../../data.json';
 
 @Component({
   selector: 'app-footer',
+  standalone: true as boolean,
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  footerData: any = (siteData as any).default;
 }
+
